9b8e9347396f1cdf15fc22ee0cd0a1453d74b02f
frantzmathieu03 <frantz.mathieu@yahoo.com>
2026-05-06 23:03:18 -0400

    Use Clerk production key

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/react'
 
-const PUBLISHABLE_KEY = 'pk_test_ZXhwZXJ0LWVzY2FyZ290LTEuY2xlcmsuYWNjb3VudHMuZGV2JA'
+const PUBLISHABLE_KEY = 'pk_live_Y2x1cmsuYXBwb21udGVhc2UuaW8k'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
